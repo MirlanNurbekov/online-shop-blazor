9059524aeee0bacd06333beab78a58832825c708
Mirlan Nurbekov <108220339+MirlanNurbekov@users.noreply.github.com>
2023-10-17 00:38:16 +0600

    Project structure and basic UI
    This is a starting point where I put together some ideas and made UI. Got already categories and some products which content is hardcoded but all redirecting and working just fine. The project is aimed to be fully functioning  online shop.

---
+++ BlazingShop/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,56 @@
+﻿using BlazingShop.Shared;
+
+namespace BlazingShop.Client.Services.ProductService
+{
+	public class ProductService : IProductService
+	{
+		public List<Product> Products { get; set; } = new List<Product>();
+
+		public void LoadProducts()
+		{
+			Products = new List<Product>
+			{
+				new Product
+				{
+					Id = 1,
+					CatergoryId = 1,
+					Title = "Luci",
+					Description ="Good movie",
+					Image = "https://mir-s3-cdn-cf.behance.net/project_modules/disp/41d3b880123243.5cd86d80c4873.jpg",
+					Price = 9.99m,
+					OriginalPrice= 19.99m,
+				},
+				new Product
+				{
+					Id = 2,
+					CatergoryId = 1,
+					Title = "Luci1212",
+					Description ="Good movie",
+					Image = "https://mir-s3-cdn-cf.behance.net/project_modules/disp/41d3b880123243.5cd86d80c4873.jpg",
+					Price = 92.99m,
+					OriginalPrice= 192.99m,
+				},
+				new Product
+				{
+					Id = 2,
+					CatergoryId = 2,
+					Title = "NOT BOOK",
+					Description ="Good movie",
+					Image = "https://mir-s3-cdn-cf.behance.net/project_modules/disp/41d3b880123243.5cd86d80c4873.jpg",
+					Price = 92.99m,
+					OriginalPrice= 192.99m,
+				},
+				new Product
+				{
+					Id = 2,
+					CatergoryId = 3,
+					Title = "Definetly not book",
+					Description ="Good movie",
+					Image = "https://mir-s3-cdn-cf.behance.net/project_modules/disp/41d3b880123243.5cd86d80c4873.jpg",
+					Price = 92.99m,
+					OriginalPrice= 192.99m,
+				},
+			};
+		}
+	}
+}
